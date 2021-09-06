9c77e89cb505db744c1ad455f752814475730fb8
Martin <alquimista3301@gmail.com>
2021-09-05 23:57:28 -0500

    feature: init login & routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import Gallery from './routes/gallery/gallery';
+import Login from './routes/login/login';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-      </header>
-      <body>
-        <Gallery></Gallery>
-      </body>
+      <nav>
+        <ul>
+          {/* <li>
+            <Link to="/">Home</Link>
+          </li> */}
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          {/* <li>
+            <Link to="/users">Users</Link>
+          </li> */}
+        </ul>
+      </nav>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/gallery">
+          <Gallery></Gallery>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        {/* <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route> */}
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/routes/login/login.js
@@ -0,0 +1,12 @@
+import './login.css';
+
+
+function Login() {
+  return (
+    <div className="">
+     <p>Login</p>
+    </div>
+  );
+}
+
+export default Login;
